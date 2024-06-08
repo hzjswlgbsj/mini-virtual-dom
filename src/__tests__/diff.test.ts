6c39faf1c7690dee9e9b1435d9ddccbd7d292847
Sixty <hiliulin@aliyun.com>
2024-06-08 15:54:16 +0800

    feat: diff 增加更复杂的测试用例,并优化增加 diff 的实现

--- src/__tests__/diff.test.ts
+++ src/__tests__/diff.test.ts
@@ -1,6 +1,9 @@
-import { diff } from '../diff/diff';
-import { PatchType } from '../types/patch';
+/**
+ * @jest-environment jsdom
+ */
 import { VNode } from '../types/vNode';
+import { PatchType } from '../types/patch';
+import { diff } from '../diff/diff';
 
 test('diff - replace node', () => {
   const oldVNode: VNode = { type: 'div', props: {}, children: [] };
@@ -10,8 +13,84 @@ test('diff - replace node', () => {
 });
 
 test('diff - props', () => {
-  const oldVNode: VNode = { type: 'div', props: { id: 'old' }, children: [] };
-  const newVNode: VNode = { type: 'div', props: { id: 'new' }, children: [] };
+  const oldVNode: VNode = { type: 'div', props: { id: 'foo' }, children: [] };
+  const newVNode: VNode = { type: 'div', props: { id: 'bar' }, children: [] };
   const patches = diff(oldVNode, newVNode);
-  expect(patches).toEqual([{ type: PatchType.PROPS, props: { id: 'new' } }]);
+  expect(patches).toEqual([{ type: PatchType.PROPS, props: { id: 'bar' } }]);
+});
+
+test('diff - children', () => {
+  const oldVNode: VNode = {
+    type: 'div',
+    props: {},
+    children: [{ type: 'p', props: {}, children: ['Old'] }],
+  };
+  const newVNode: VNode = {
+    type: 'div',
+    props: {},
+    children: [{ type: 'p', props: {}, children: ['New'] }],
+  };
+  const patches = diff(oldVNode, newVNode);
+  expect(patches).toEqual([
+    {
+      type: PatchType.CHILDREN,
+      children: [
+        {
+          type: PatchType.UPDATE,
+          index: 0,
+          patches: [
+            {
+              type: PatchType.CHILDREN,
+              children: [
+                {
+                  type: PatchType.REPLACE,
+                  newVNode: 'New',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+});
+
+test('diff - children and props', () => {
+  const oldVNode: VNode = {
+    type: 'div',
+    props: { id: 'old' },
+    children: [{ type: 'p', props: {}, children: ['Old'] }],
+  };
+  const newVNode: VNode = {
+    type: 'div',
+    props: { id: 'new' },
+    children: [{ type: 'p', props: {}, children: ['New'] }],
+  };
+  const patches = diff(oldVNode, newVNode);
+  expect(patches).toEqual([
+    {
+      type: PatchType.PROPS,
+      props: { id: 'new' },
+    },
+    {
+      type: PatchType.CHILDREN,
+      children: [
+        {
+          type: PatchType.UPDATE,
+          index: 0,
+          patches: [
+            {
+              type: PatchType.CHILDREN,
+              children: [
+                {
+                  type: PatchType.REPLACE,
+                  newVNode: 'New',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ]);
 });
--- src/types/patch.ts
+++ src/types/patch.ts
@@ -1,12 +1,21 @@
 import { VNode } from './vNode';
 
 export enum PatchType {
-  REPLACE,
-  PROPS,
-  CHILDREN,
+  /** 替换整个节点 */
+  REPLACE = 'REPLACE',
+  /** 更新节点的属性 */
+  PROPS = 'PROPS',
+  /** 更新节点的子节点 */
+  CHILDREN = 'CHILDREN',
+  /** 更新具体的子节点 */
+  UPDATE = 'UPDATE',
+  /** 移除节点 */
+  REMOVE = 'REMOVE',
 }
 
 export type Patch =
-  | { type: PatchType.REPLACE; newVNode?: VNode }
+  | { type: PatchType.REPLACE; newVNode?: VNode | string }
   | { type: PatchType.PROPS; props: { [key: string]: any } }
-  | { type: PatchType.CHILDREN; children: Patch[] };
+  | { type: PatchType.CHILDREN; children: Patch[] }
+  | { type: PatchType.UPDATE; index: number; patches: Patch[] }
+  | { type: PatchType.REMOVE };
